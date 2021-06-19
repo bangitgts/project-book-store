5ffe0349c6ab38f47dcdfd868c595d481d5250b5
bangitgts <bangitgts@gmail.com>
2021-06-20 05:05:52 +0700

    listbook v1

--- src/components/ListBook/ListBook.jsx
+++ src/components/ListBook/ListBook.jsx
@@ -5,344 +5,53 @@ class ListBook extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
     };
   }
   componentDidMount() {
     var config = {
       method: "get",
       url: "http://45.77.12.16:4000/product",
-   
     };
 
     axios(config)
-     .then(req => console.log(req.data))
+      .then((req) => req.data.data)
+      .then((data) => {
+        this.setState({
+          data: data,
+        });
+      });
   }
   render() {
+    const { data } = this.state;
+    const dataList = data.map((data) => {
+      
+      return (
+        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3 ">
+          <div className="product-image-wrapper">
+            <div className="single-products">
+              <div className="productinfo">
+                <div className="product-background text-center">
+                  <img
+                    className="text-center"
+                    src= {data.urlImage}
+                    alt=""
+                  />
+                </div>
+                <h4 className="pdl-5">{data.tenSach}</h4>
+                <p className="pdl-5">Tác giả: {data.tacGia}</p>
+                <h4 className="pdl-5">Giá bìa: {parseInt(data.giaBia).toLocaleString()} đ</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    });
     return (
       <div className="features_items">
         {/*features_items*/}
-        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3 ">
-          <div className="product-image-wrapper">
-            <div className="single-products">
-              <div className="productinfo ">
-                <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src="http://skybooks.vn/wp-content/themes/skybooks/thumb.php?src=/wp-content/uploads/2021/06/t%E1%BA%A3i-xu%E1%BB%91ng.jpg&w=226&h=366&q=100"
-                    alt=""
-                  />
-                </div>
-                <h4 className="pdl-5">Lúc nào cũng là quá muộn</h4>
-                <p className="pdl-5">Tác giả: abcxyz</p>
-                <h4 className="pdl-5">92,000 ₫</h4>
-                {/* <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
-              </div>
-              <div className="product-overlay">
-                <div className="overlay-content">
-                  <h2>$56</h2>
-                  <p>Easy Polo Black Edition</p>
-                  <a href="#" className="btn btn-default add-to-cart">
-                    <i className="fa fa-shopping-cart" />
-                    Add to cart
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div class="choose">
-                            <ul class="nav nav-pills nav-justified">
-                                <li>
-                                    <a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a
-                          >
-                        </li>
-                        <li>
-                          <a href="#"><i class="fa fa-plus-square"></i>Add to compare</a>
-                                </li>
-                            </ul>
-                        </div> */}
-          </div>
-        </div>
-        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-          <div className="product-image-wrapper">
-            <div className="single-products">
-              <div className="productinfo ">
-                <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src="http://skybooks.vn/wp-content/themes/skybooks/thumb.php?src=/wp-content/uploads/2021/06/t%E1%BA%A3i-xu%E1%BB%91ng.jpg&w=226&h=366&q=100"
-                    alt=""
-                  />
-                </div>
-                <h4 className="pdl-5">Lúc nào cũng là quá muộn</h4>
-                <p className="pdl-5">Tác giả: abcxyz</p>
-                <h4 className="pdl-5">92,000 ₫</h4>
-                {/* <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
-              </div>
-              <div className="product-overlay">
-                <div className="overlay-content">
-                  <h2>$56</h2>
-                  <p>Easy Polo Black Edition</p>
-                  <a href="#" className="btn btn-default add-to-cart">
-                    <i className="fa fa-shopping-cart" />
-                    Add to cart
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div class="choose">
-                          <ul class="nav nav-pills nav-justified">
-                              <li>
-                                  <a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a
-                        >
-                      </li>
-                      <li>
-                        <a href="#"><i class="fa fa-plus-square"></i>Add to compare</a>
-                              </li>
-                          </ul>
-                      </div> */}
-          </div>
-        </div>
-        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-          <div className="product-image-wrapper">
-            <div className="single-products">
-              <div className="productinfo ">
-                <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src="http://skybooks.vn/wp-content/themes/skybooks/thumb.php?src=/wp-content/uploads/2021/06/t%E1%BA%A3i-xu%E1%BB%91ng.jpg&w=226&h=366&q=100"
-                    alt=""
-                  />
-                </div>
-                <h4 className="pdl-5">Lúc nào cũng là quá muộn</h4>
-                <p className="pdl-5">Tác giả: abcxyz</p>
-                <h4 className="pdl-5">92,000 ₫</h4>
-                {/* <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
-              </div>
-              <div className="product-overlay">
-                <div className="overlay-content">
-                  <h2>$56</h2>
-                  <p>Easy Polo Black Edition</p>
-                  <a href="#" className="btn btn-default add-to-cart">
-                    <i className="fa fa-shopping-cart" />
-                    Add to cart
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div class="choose">
-                        <ul class="nav nav-pills nav-justified">
-                            <li>
-                                <a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a
-                      >
-                    </li>
-                    <li>
-                      <a href="#"><i class="fa fa-plus-square"></i>Add to compare</a>
-                            </li>
-                        </ul>
-                    </div> */}
-          </div>
-        </div>
-        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-          <div className="product-image-wrapper">
-            <div className="single-products">
-              <div className="productinfo ">
-                <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src="http://skybooks.vn/wp-content/themes/skybooks/thumb.php?src=/wp-content/uploads/2021/06/t%E1%BA%A3i-xu%E1%BB%91ng.jpg&w=226&h=366&q=100"
-                    alt=""
-                  />
-                </div>
-                <h4 className="pdl-5">Lúc nào cũng là quá muộn</h4>
-                <p className="pdl-5">Tác giả: abcxyz</p>
-                <h4 className="pdl-5">92,000 ₫</h4>
-                {/* <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
-              </div>
-              <div className="product-overlay">
-                <div className="overlay-content">
-                  <h2>$56</h2>
-                  <p>Easy Polo Black Edition</p>
-                  <a href="#" className="btn btn-default add-to-cart">
-                    <i className="fa fa-shopping-cart" />
-                    Add to cart
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div class="choose">
-                      <ul class="nav nav-pills nav-justified">
-                          <li>
-                              <a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a
-                    >
-                  </li>
-                  <li>
-                    <a href="#"><i class="fa fa-plus-square"></i>Add to compare</a>
-                          </li>
-                      </ul>
-                  </div> */}
-          </div>
-        </div>
-        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3">
-          <div className="product-image-wrapper">
-            <div className="single-products">
-              <div className="productinfo ">
-                <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src="http://skybooks.vn/wp-content/themes/skybooks/thumb.php?src=/wp-content/uploads/2021/06/t%E1%BA%A3i-xu%E1%BB%91ng.jpg&w=226&h=366&q=100"
-                    alt=""
-                  />
-                </div>
-                <h4 className="pdl-5">Lúc nào cũng là quá muộn</h4>
-                <p className="pdl-5">Tác giả: abcxyz</p>
-                <h4 className="pdl-5">92,000 ₫</h4>
-                {/* <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
-              </div>
-              <div className="product-overlay">
-                <div className="overlay-content">
-                  <h2>$56</h2>
-                  <p>Easy Polo Black Edition</p>
-                  <a href="#" className="btn btn-default add-to-cart">
-                    <i className="fa fa-shopping-cart" />
-                    Add to cart
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div class="choose">
-                    <ul class="nav nav-pills nav-justified">
-                        <li>
-                            <a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a
-                  >
-                </li>
-                <li>
-                  <a href="#"><i class="fa fa-plus-square"></i>Add to compare</a>
-                        </li>
-                    </ul>
-                </div> */}
-          </div>
-        </div>
-        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3 ">
-          <div className="product-image-wrapper">
-            <div className="single-products">
-              <div className="productinfo ">
-                <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src="http://skybooks.vn/wp-content/themes/skybooks/thumb.php?src=/wp-content/uploads/2021/06/t%E1%BA%A3i-xu%E1%BB%91ng.jpg&w=226&h=366&q=100"
-                    alt=""
-                  />
-                </div>
-                <h4 className="pdl-5">Lúc nào cũng là quá muộn</h4>
-                <p className="pdl-5">Tác giả: abcxyz</p>
-                <h4 className="pdl-5">92,000 ₫</h4>
-                {/* <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
-              </div>
-              <div className="product-overlay">
-                <div className="overlay-content">
-                  <h2>$56</h2>
-                  <p>Easy Polo Black Edition</p>
-                  <a href="#" className="btn btn-default add-to-cart">
-                    <i className="fa fa-shopping-cart" />
-                    Add to cart
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div class="choose">
-                  <ul class="nav nav-pills nav-justified">
-                      <li>
-                          <a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a
-                >
-              </li>
-              <li>
-                <a href="#"><i class="fa fa-plus-square"></i>Add to compare</a>
-                      </li>
-                  </ul>
-              </div> */}
-          </div>
-        </div>
-        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3 ">
-          <div className="product-image-wrapper">
-            <div className="single-products">
-              <div className="productinfo ">
-                <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src="http://skybooks.vn/wp-content/themes/skybooks/thumb.php?src=/wp-content/uploads/2021/06/t%E1%BA%A3i-xu%E1%BB%91ng.jpg&w=226&h=366&q=100"
-                    alt=""
-                  />
-                </div>
-                <h4 className="pdl-5">Lúc nào cũng là quá muộn</h4>
-                <p className="pdl-5">Tác giả: abcxyz</p>
-                <h4 className="pdl-5">92,000 ₫</h4>
-                {/* <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
-              </div>
-              <div className="product-overlay">
-                <div className="overlay-content">
-                  <h2>$56</h2>
-                  <p>Easy Polo Black Edition</p>
-                  <a href="#" className="btn btn-default add-to-cart">
-                    <i className="fa fa-shopping-cart" />
-                    Add to cart
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div class="choose">
-                <ul class="nav nav-pills nav-justified">
-                    <li>
-                        <a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a
-              >
-            </li>
-            <li>
-              <a href="#"><i class="fa fa-plus-square"></i>Add to compare</a>
-                    </li>
-                </ul>
-            </div> */}
-          </div>
-        </div>
-        <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3 ">
-          <div className="product-image-wrapper">
-            <div className="single-products">
-              <div className="productinfo ">
-                <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src="http://skybooks.vn/wp-content/themes/skybooks/thumb.php?src=/wp-content/uploads/2021/06/t%E1%BA%A3i-xu%E1%BB%91ng.jpg&w=226&h=366&q=100"
-                    alt=""
-                  />
-                </div>
-                <h4 className="pdl-5">Lúc nào cũng là quá muộn</h4>
-                <p className="pdl-5">Tác giả: abcxyz</p>
-                <h4 className="pdl-5">92,000 ₫</h4>
-                {/* <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a> */}
-              </div>
-              <div className="product-overlay">
-                <div className="overlay-content">
-                  <h2>$56</h2>
-                  <p>Easy Polo Black Edition</p>
-                  <a href="#" className="btn btn-default add-to-cart">
-                    <i className="fa fa-shopping-cart" />
-                    Add to cart
-                  </a>
-                </div>
-              </div>
-            </div>
-            {/* <div class="choose">
-              <ul class="nav nav-pills nav-justified">
-                  <li>
-                      <a href="#"><i class="fa fa-plus-square"></i>Thêm vào danh sách yêu thích</a
-            >
-          </li>
-          <li>
-            <a href="#"><i class="fa fa-plus-square"></i>Add to compare</a>
-                  </li>
-              </ul>
-          </div> */}
-          </div>
-        </div>
-      </div>
+        {dataList}
+     </div>   
     );
   }
 }
