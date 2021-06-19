2e65531fdd1e7c10097c0478a114efec84daab1c
bangitgts <bangitgts@gmail.com>
2021-06-20 05:13:16 +0700

    fix router

--- src/components/ListBook/ListBook.jsx
+++ src/components/ListBook/ListBook.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable import/first */
+/* eslint-disable jsx-a11y/anchor-has-content */
 import React from "react";
 const axios = require("axios");
-
+import { Link } from "react-router-dom";
 class ListBook extends React.Component {
   constructor(props) {
     super(props);
@@ -25,22 +27,21 @@ class ListBook extends React.Component {
   render() {
     const { data } = this.state;
     const dataList = data.map((data) => {
-      
       return (
         <div className="col-xs-6 col-sm-3 col-md-3 col-lg-3 ">
           <div className="product-image-wrapper">
             <div className="single-products">
               <div className="productinfo">
                 <div className="product-background text-center">
-                  <img
-                    className="text-center"
-                    src= {data.urlImage}
-                    alt=""
-                  />
+                  <Link to="/login">
+                    <img className="text-center" src={data.urlImage} alt="" />
+                  </Link>
                 </div>
                 <h4 className="pdl-5">{data.tenSach}</h4>
                 <p className="pdl-5">Tác giả: {data.tacGia}</p>
-                <h4 className="pdl-5">Giá bìa: {parseInt(data.giaBia).toLocaleString()} đ</h4>
+                <h4 className="pdl-5">
+                  Giá bìa: {parseInt(data.giaBia).toLocaleString()} đ
+                </h4>
               </div>
             </div>
           </div>
@@ -51,7 +52,7 @@ class ListBook extends React.Component {
       <div className="features_items">
         {/*features_items*/}
         {dataList}
-     </div>   
+      </div>
     );
   }
 }
