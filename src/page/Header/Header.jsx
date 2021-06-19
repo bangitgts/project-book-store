e11c87ffa864d01586b7543070d5df9ee221199f
bangitgts <bangitgts@gmail.com>
2021-06-20 04:12:04 +0700

    done header top

--- src/page/Header/Header.jsx
+++ src/page/Header/Header.jsx
@@ -1,53 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
+import { HeaderTop } from "../HeaderTop";
 class Header extends React.Component {
   render() {
     return (
       <header id="header">
         {/*header*/}
-        <div className="header_top">
-          {/*header_top*/}
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-6">
-                <div className="contactinfo">
-                  <ul className="nav nav-pills">
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-phone" /> +123456789
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-envelope" /> info@xyz.com
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-sm-6">
-                <div className="social-icons pull-right">
-                  <ul className="nav navbar-nav">
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-facebook" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-twitter" />
-                      </a>
-                    </li>
-                    <li>
-                      <a href="#">
-                        <i className="fa fa-youtube" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <HeaderTop/>
         {/*/header_top*/}
         <div className="header-middle">
           {/*header-middle*/}
---
+++ src/page/HeaderTop/HeaderTop.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+class HeaderTop extends React.Component {
+  render() {
+    return (
+      <div className="header_top">
+        {/*header_top*/}
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="contactinfo">
+                <ul className="nav nav-pills">
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-phone" /> +123456789
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-envelope" /> info@xyz.com
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="social-icons pull-right">
+                <ul className="nav navbar-nav">
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-facebook" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-twitter" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <i className="fa fa-youtube" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export { HeaderTop };
