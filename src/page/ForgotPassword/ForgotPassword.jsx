80d07f293455a2f209070a2db7e572b2a0f2d9da
bangitgts <bangitgts@gmail.com>
2021-06-28 01:13:08 +0700

    doi forgot password

--- src/page/ForgotPassword/ForgotPassword.jsx
+++ src/page/ForgotPassword/ForgotPassword.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Redirect } from "react-router-dom";
 import ReactNotification from "react-notifications-component";
 import "react-notifications-component/dist/theme.css";
 import ReCAPTCHA from "react-google-recaptcha";
@@ -13,6 +13,7 @@ class ForgotPassword extends React.Component {
     super(props);
     this.state = {
       email: "",
+      redirect: false,
       isVerified: false,
     };
     this.onLoadRecaptcha = this.onLoadRecaptcha.bind(this);
@@ -46,13 +47,82 @@ class ForgotPassword extends React.Component {
       });
     }
   }
-
+  Redirect() {
+    console.log("1");
+    window.location = "http://www.vietjack.com";
+  }
   handleSubmit(e) {
+    console.log(this.state.email);
     e.preventDefault();
-    
+    if (this.state.isVerified === false) {
+      store.addNotification({
+        title: "Chưa xác nhận captcha!",
+        message: "Vui lòng xác nhận captcha",
+        type: "danger",
+        insert: "top",
+        container: "top-center",
+        dismiss: {
+          duration: 10000,
+          onScreen: true,
+          showIcon: true,
+        },
+      });
+    } else {
+      var data = qs.stringify({
+        email: this.state.email,
+      });
+      var config = {
+        method: "post",
+        url: "http://45.77.12.16:4000/account/forgotpassword",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: data,
+      };
+
+      axios(config)
+        .then(function (response) {
+          return response;
+        })
+        .then((data) => {
+          this.setState({
+            redirect: true,
+          });
+          store.addNotification({
+            title: "Đng chuyển!",
+            message: "Vui lòng nhập chính xác tài khoản",
+            type: "danger",
+            insert: "top",
+            container: "top-center",
+            dismiss: {
+              duration: 10000,
+              onScreen: true,
+              showIcon: true,
+            },
+          });
+        })
+        .catch(function (error) {
+          console.log(error);
+          store.addNotification({
+            title: "Tài khoản này chưa đăng ký!",
+            message: "Vui lòng nhập chính xác tài khoản",
+            type: "danger",
+            insert: "top",
+            container: "top-center",
+            dismiss: {
+              duration: 10000,
+              onScreen: true,
+              showIcon: true,
+            },
+          });
+        });
+    }
   }
 
   render() {
+    if (this.state.redirect === true) {
+      return <Redirect to="/" />;
+    }
     return (
       <div className="page-content page-login">
         <ReactNotification />
@@ -68,7 +138,6 @@ class ForgotPassword extends React.Component {
                         Nhập email của bạn để lấy lại mật khẩu
                       </p>
                       <form className="m-t-md" onSubmit={this.handleSubmit}>
-                        
                         <div className="form-group">
                           <input
                             type="email"
@@ -79,7 +148,6 @@ class ForgotPassword extends React.Component {
                             required
                           />
                         </div>
-                        
                         <ReCAPTCHA
                           sitekey="6Ld55F0bAAAAAE2JZ9lo1razwPyfUiaMEvBvDbUV"
                           onChange={this.onChangeCaptcha}
@@ -92,14 +160,8 @@ class ForgotPassword extends React.Component {
                           type="submit"
                           className="btn btn-login btn-block"
                         >
-                          Đăng ký
+                          Lấy lại mật khẩu
                         </button>
-                        <a
-                          href="forgotpassword"
-                          className="display-block text-center m-t-md text-sm"
-                        >
-                          Quên mật khẩu?
-                        </a>
                         <a
                           href="login"
                           className="btn btn-default btn-block m-t-md"
